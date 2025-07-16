e8f83bc8457e4cfe96e968a6835a3cdb87a8b110
André <andre_miguel_costa@hotmail.com>
2025-07-16 09:57:34 +0200

    feat: create rpi4 board with sdl and wayland configs

---
+++ rpi4/src/main.c
@@ -0,0 +1,31 @@
+#include <lvgl.h>
+#include <lv_demos.h>
+#include <unistd.h>
+
+void on_benchmark_end(const lv_demo_benchmark_summary_t *summary)
+{
+	lv_demo_benchmark_summary_display(summary);
+	exit(0);
+}
+
+int main(void)
+{
+	lv_init();
+#if LV_USE_SDL
+	lv_sdl_window_create(1920, 1080);
+#elif LV_USE_WAYLAND
+	lv_wayland_window_create(1920, 1080, "EJ LVGL Benchmark", NULL);
+#endif
+
+	lv_demo_benchmark_set_end_cb(on_benchmark_end);
+	lv_demo_benchmark();
+
+	while (1) {
+		const uint32_t time_to_next = lv_timer_handler();
+		if (time_to_next == LV_NO_TIMER_READY) {
+			usleep(LV_DEF_REFR_PERIOD * 1000);
+		} else {
+			usleep(time_to_next * 1000);
+		}
+	}
+}
