8e6525d936f007bd2cbb3a993a1411d89da248e7
André <andre_miguel_costa@hotmail.com>
2025-07-16 13:31:34 +0200

    fix(rpi4): call wayland timer handler

--- rpi4/src/main.c
+++ rpi4/src/main.c
@@ -14,15 +14,20 @@ int main(void)
 #if LV_USE_SDL
 	lv_sdl_window_create(1920, 1080);
 #elif LV_USE_WAYLAND
-	lv_display_t * disp = lv_wayland_window_create(1920, 1080, "EJ LVGL Benchmark", NULL);
-	lv_wayland_window_set_maximized(disp, true);
+	lv_display_t *disp = lv_wayland_window_create(1920, 1080, "EJ LVGL Benchmark", NULL);
+	lv_wayland_window_set_maximized(disp, false);
 #endif
 
 	lv_demo_benchmark_set_end_cb(on_benchmark_end);
 	lv_demo_benchmark();
 
 	while (1) {
+#if LV_USE_WAYLAND
+		const uint32_t time_to_next = lv_wayland_timer_handler();
+#else
 		const uint32_t time_to_next = lv_timer_handler();
+#endif
+
 		if (time_to_next == LV_NO_TIMER_READY) {
 			usleep(LV_DEF_REFR_PERIOD * 1000);
 		} else {
